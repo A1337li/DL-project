565ad5f8df1a786e6c571e1722af79178a381282
gedholm <gustav@edholm.se>
2019-04-29 14:19:56 +0200

    csv_reader changed to return dictionary

--- csv_reader.py
+++ csv_reader.py
@@ -8,11 +8,13 @@ def get_labels(file_name):
     with open(file_name, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         text_labels = []
-        labels = []
+        files_and_labels = {}
         for row in csv_reader:
             text_label = row["classification"]
+            title = row["image.name"]
+            image_file_name = title + ".jpg"
             if not text_label in text_labels:
                 text_labels.append (text_label)
             label = text_labels.index(text_label)
-            labels.append(label)
-    return(labels)
+            files_and_labels[image_file_name] = label
+    return(files_and_labels)
