d30e939b16aaa8edfaba2234e9bdaa6c5ba80910
gedholm <gustav@edholm.se>
2019-04-29 14:41:35 +0200

    changed to 3 labels, and print percentages

--- csv_reader.py
+++ csv_reader.py
@@ -7,7 +7,6 @@ labels = []
 def get_labels(file_name):
     with open(file_name, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        text_labels = []
         files_and_labels = {}
         for row in csv_reader:
             text_label = row["classification"]
@@ -16,5 +15,7 @@ def get_labels(file_name):
             if not text_label in text_labels:
                 text_labels.append (text_label)
             label = text_labels.index(text_label)
+            if label == 3:
+                label = 2
             files_and_labels[image_file_name] = label
     return(files_and_labels)
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from csv_reader import get_labels
 
 labels = get_labels("Data_Osteo_Tiles/ML_Features_1144.csv")
-print(labels)
+label_counter = [0]*3
+
+for label in labels.values():
+	label_counter[label] += 1
+
+print("label percentages: ")
+for counter in label_counter:
+	print(counter/len(labels))
+
