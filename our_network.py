b96a5f68a524f9f57948b7c75c076416439cace2
Tjocksockar <Tjocksockar@gmail.com>
2019-05-16 15:28:07 +0200

    out network

---
+++ our_network.py
@@ -0,0 +1,35 @@
+import tensorflow as tf 
+
+from tensorflow.python.keras.models import Model, Sequential
+from tensorflow.python.keras.layers import Dense, Flatten, Dropout, Activation, Conv2D, MaxPooling2D
+
+def create_our_model(): 
+
+	# BLOCK 1
+	model = Sequential()
+	model.add(Conv2D(32, (3, 3), input_shape = [224, 224, 3]))
+		# arg1 = n_filters, arg2 = filter size, arg3 = input size for making it dynamic 
+	model.add(Conv2D(32, (3, 3)))
+	#model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size = (2, 2)))
+	
+	# BLOCK 2
+	model.add(Conv2D(64, (3, 3)))
+	model.add(Conv2D(64, (3, 3)))
+	#model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size = (2, 2)))
+
+	# BLOCK 3
+	model.add(Conv2D(128, (3, 3)))
+	model.add(Conv2D(128, (3, 3)))
+	model.add(Conv2D(128, (3, 3)))
+	#model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size = (2, 2)))
+
+	model.add(Flatten())
+	model.add(Dense(256, activation = 'relu'))
+	#model.add(Dense(128, activation = 'relu'))
+	model.add(Dense(3, activation='softmax')) # 3 stands for number of classes
+
+	return model
+
